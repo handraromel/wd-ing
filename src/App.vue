38539b7c9449a7f98d3edec16db04504849a0bfc
handraromel <handromel@gmail.com>
2022-10-08 14:46:54 +0700

    update header

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,36 @@
 <script>
 import Header from "./components/Header.vue";
-import Navbar from "./components/Navbar.vue"
+// import Navbar from "./components/Navbar.vue"
 export default {
   components: {
     Header,
-    Navbar
+    // Navbar
   },
 };
 </script>
 
 <template>
   <div id="app">
-    <Navbar />
+    <!-- <Navbar /> -->
     <Header />
     <main></main>
   </div>
 </template>
 
 <style scoped>
-  @font-face {
-    font-family: "nunito";
-    src: url(@/assets/fonts/nunito.ttf);
-  }
+@font-face {
+  font-family: "nunito";
+  src: url(@/assets/fonts/nunito.ttf);
+}
+@font-face {
+  font-family: "fajardo";
+  src: url(@/assets/fonts/fajardo.ttf);
+}
 
-  #app {
-    font-family: "nunito";
-    color: #7c6a5e;
-  }
+#app {
+  font-family: "nunito";
+  color: #7c6a5e;
+  background: url(@/assets/img/header-bg.webp) center no-repeat;
+  background-size: cover;
+}
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,23 +3,23 @@
     <div class="container pb-md-0">
       <div
         id="header-content"
-        class="row align-items-lg-center vh-100 pt-top"
+        class="row justify-content-center align-items-lg-center pt-5 pt-lg-0"
       >
-        <div class="col-xl-6 text-center px-sm-5 px-3">
+        <div class="col-xl-6 text-center px-sm-5 px-3 pt-lg-5">
           <img
             class="img-fluid"
             src="@/assets/img/headec1.png"
             alt="Decoration 1"
             draggable="false"
           />
-          <div class="py-5">
-            <h1 class="fw-bold">Lorem The Ipsum</h1>
+          <div class="py-4">
+            <h1 class="fw-bold m-0">Lorem The Ipsum</h1>
             <h3 style="letter-spacing: 0.4em">Lorem &amp; Ipsum</h3>
           </div>
           <a href="javascript:;" class="btn rounded-pill read-more"
             >READ MORE</a
           >
-          <div class="pt-5">
+          <div class="py-5">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean non
             dignissim neque. Phasellus ullamcorper libero tellus, in aliquet
             elit placerat interdum. Vivamus porttitor turpis neque, et porttitor
@@ -27,21 +27,21 @@
             eros ut felis eleifend, quis ultricies felis posuere.
           </div>
         </div>
-        <div class="col-xl-6 text-center position-relative p-5">
+        <div class="col-xl-6 text-center position-relative px-5">
           <img
             class="img-fluid"
             src="@/assets/img/sample.webp"
             alt="Sample Photo"
             draggable="false"
           />
-          <div class="dec-tape">
+          <!-- <div class="dec-tape">
             <img
               class="img-fluid"
               src="@/assets/img/headec2.png"
               alt="Decoration 2"
               draggable="false"
             />
-          </div>
+          </div> -->
           <div class="dec-logo">
             <img
               class="img-fluid"
@@ -59,8 +59,12 @@
             />
           </div>
         </div>
+        <div class="col-xl-6 text-center pt-about">
+          <a href="javascript:;" style="color: inherit; font-family: fajardo">
+            <span class="fs-1">Tentang kami</span>
+          </a>
+        </div>
       </div>
-      <div class="bottom-padding"></div>
     </div>
   </header>
 </template>
@@ -71,15 +75,6 @@ $shadowRadius: 1px 5px 15px #7c6a5e;
 $baseColor: #7c6a5e;
 $whiteText: #fff;
 $buttonPadding: 0.4em 3em;
-
-#wd-header {
-  background: url(#{$assetsPath}/header-bg.webp) center no-repeat;
-  background-size: cover;
-}
-
-#header-content {
-  min-height: 100vh;
-}
 
 .read-more {
   background: $baseColor;
@@ -113,18 +108,38 @@ $buttonPadding: 0.4em 3em;
   }
 }
 
-@include decor-position("tape", top, 0, left, 0, width, 25%);
-@include decor-position("logo", top, 15%, right, 0, width, 30%);
-@include decor-position("loc", bottom, -10%, right, 15%, width, 55%);
+@mixin headerHeight($height) {
+  #header-content {
+    min-height: $height;
+  }
+}
 
-@media (max-width: 1199px) {
+@mixin aboutUsPadding($pt, $pb) {
+  .pt-about {
+    padding-top: $pt;
+    padding-bottom: $pb;
+  }
+}
+
+@include headerHeight(100vh);
+
+// @include decor-position("tape", top, 0, left, 0, width, 25%);
+@include decor-position("logo", top, 5%, right, 0, width, 30%);
+@include decor-position("loc", bottom, -30%, right, 15%, width, 55%);
+
+@media (max-width: 1200px) {
+  @include headerHeight(85vh);
+  @include aboutUsPadding(14em, 5em);
+}
+
+@media (max-width: 760px) {
   @include decor-position("tape", top, 5%, left, 0, width, 25%);
+  @include headerHeight(85vh);
 
   .bottom-padding {
-    padding-bottom: 7em;
+    padding-bottom: 5em;
   }
-  .pt-top {
-    padding-top: 6em;
-  }
+
+  @include aboutUsPadding(7em, 5em);
 }
 </style>
